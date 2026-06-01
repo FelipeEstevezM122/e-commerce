0f6cc79fca822db0016a2412ae04bd899fd30277
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-01 17:44:29 -0400

    Se agrego CartItem(Los productos que estan en el carrito) con sus respectivas funciones

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'cart_id', //id de su carrito
+        'product_id', //producto
+        'quantity', //cantidad del producto
+        'price_when_added' //precio del producto
+    ];
+
+    //Relacion: Un item pertenece a 1 carrito
+    public function cart(){
+        return $this->belongsTo(Cart::class);
+    }
+
+    //Relacion: Un item pertenece a 1 producto
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    //Total del item (precio * cantidad)
+    public function getTotalAttribute(){
+        return $this->price_when_added * $this->quantity;
+    }
 }
