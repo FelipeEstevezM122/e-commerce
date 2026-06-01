ffaf92c07d4366c40607cbda255e0290586089e1
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-01 17:14:17 -0400

    Modelo Rank con sus funciones

--- app/Models/Rank.php
+++ app/Models/Rank.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Rank extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'monthly_minimum_purchase', //Compra minima mensual
+        'description',
+    ];
+
+    //Relacion: Un rango esta en N usuarios
+    public function users(){
+        return $this->hasMany(User::class);
+    }
+
+    //Verificar la compra minima
+    public function qualifies($totalPurchases){
+        return $totalPurchases >= $this->monthly_minimum_purchase;
+    }
 }
