463ef6d344328a7c8b74a45023cacaf7ac6712fc
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-01 18:48:45 -0400

    Se agrego el modelo Ticket(de la compro) con sus respectivas funciones

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'ticket_number', //numero unico del ticket
+        'issued_at'// fecha de emisión
+    ];
+
+    protected $casts = [
+        'issued_at' => 'datetime'
+    ];
+
+    //Relacion: Un ticket pertenece a 1 pedido
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    //Generar un numero de ticket automaticamente
+    public static function generateTicketNumber()
+    {
+        $last = self::latest('id')->first();
+        $number = $last ? intval(substr($last->ticket_number, 4)) + 1 : 1;
+        return 'TKT-' . str_pad($number, 8, '0', STR_PAD_LEFT);
+    }
 }
