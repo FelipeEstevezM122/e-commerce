efbec28074a769339af558a8030ec5a7f46875a1
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-01 17:21:04 -0400

    Se agrego el modelo de Bran(Marca) con sus funciones

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'logo',
+    ];
+
+    //Relacion: Una marcaa tiene N productos
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
