06f45b8c365542319e3c5a13ea8a1786302ea5b5
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-01 18:43:24 -0400

    Se agrego el modelo OrderItem(Productos del pedido) con sus respectivas funcionas

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity', //cantidad del producto
+        'price_when_ordered' //precio del producto
+    ];
+
+    //Relacion: Un item pertenece a un pedido
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    
+    //Relacion: Un item pertenece a un producto
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    //Calcular el subtotal del item (precio × cantidad)
+    public function getSubtotalAttribute()
+    {
+        return $this->price_when_purchased * $this->quantity;
+    }
 }
