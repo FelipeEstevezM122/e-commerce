3cbbfc660548128e0c573590c011c25a6e389eb5
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-02 17:18:32 -0400

    Correccion migracion Items del carrito

--- database/migrations/2026_06_01_202426_create_cart_items_table.php
+++ database/migrations/2026_06_01_202426_create_cart_items_table.php
@@ -6,24 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('cart_id')->constrained('carts')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity')->default(1);
+            $table->decimal('price_when_added', 10, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('cart_items');
