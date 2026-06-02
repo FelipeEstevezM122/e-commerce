ae58ef0a0064b738b6a7c495e48755dd2f5b9a85
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-02 17:47:41 -0400

    Eliminacion de comentario

--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -2,22 +2,19 @@
 
 namespace Database\Factories;
 
+use App\Models\Brand;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
- */
 class BrandFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Brand::class;
+
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->unique()->company(),
+            'description' => $this->faker->optional()->paragraph(),
+            'logo' => $this->faker->optional()->imageUrl(200, 200, 'brands'),
         ];
     }
 }
