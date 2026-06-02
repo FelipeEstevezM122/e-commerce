43ec80db8d9b11d612e998427dcb5d63bc812b61
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-02 17:23:45 -0400

    Correccion migracion Tickets

--- database/migrations/2026_06_01_202450_create_tickets_table.php
+++ database/migrations/2026_06_01_202450_create_tickets_table.php
@@ -6,24 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->unique()->constrained('orders')->onDelete('cascade');
+            $table->string('ticket_number', 50)->unique();
+            $table->timestamp('issued_at')->default(now());
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('tickets');
