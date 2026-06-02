d0bd13321df926318b6cff7223526594e049cdff
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-02 17:16:05 -0400

    Correccion migracion Productos

--- database/migrations/2026_06_01_202258_create_products_table.php
+++ database/migrations/2026_06_01_202258_create_products_table.php
@@ -6,24 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150);
+            $table->text('description')->nullable();
+            $table->decimal('base_price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->string('image', 255)->nullable();
+            $table->string('sku', 50)->unique();
+            $table->integer('warranty_days')->nullable();
+            $table->foreignId('brand_id')->nullable()->constrained('brands')->nullOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained('categories')->nullOnDelete();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('products');
