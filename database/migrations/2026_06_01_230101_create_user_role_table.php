fde967ed13f7979189e5d7b2a417fe8bba546531
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-02 17:25:20 -0400

    Correccion migracion Roles

--- database/migrations/2026_06_01_230101_create_user_role_table.php
+++ database/migrations/2026_06_01_230101_create_user_role_table.php
@@ -10,12 +10,10 @@ return new class extends Migration
     {
         Schema::create('user_role', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->date('assigned_at')->default(now());
             $table->timestamps();
-            
-            // Un usuario no puede tener el mismo rol dos veces
             $table->unique(['user_id', 'role_id']);
         });
     }
