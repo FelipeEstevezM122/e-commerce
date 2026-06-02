293529c5263bf27aa7768de031a286c6d69fe8de
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-02 17:22:56 -0400

    Correccion migracion Items de Pedido

--- database/migrations/2026_06_01_202444_create_order_items_table.php
+++ database/migrations/2026_06_01_202444_create_order_items_table.php
@@ -6,24 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('price_when_ordered', 10, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('order_items');
