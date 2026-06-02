8be89ecf9edbed6ff96ac33caf6d1a5d77cbd35f
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-02 17:24:44 -0400

    Correccion migracion Compras Acumuladas

--- database/migrations/2026_06_01_204728_create_accumulated_purchases_table.php
+++ database/migrations/2026_06_01_204728_create_accumulated_purchases_table.php
@@ -6,24 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('accumulated_purchases', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->date('month');
+            $table->date('quarter');
+            $table->date('year');
+            $table->decimal('total_purchases', 10, 2)->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('accumulated_purchases');
