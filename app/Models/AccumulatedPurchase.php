a844211a2a86b2369a7b1e897ffc3cb445baf589
SandiaCamil <cussisuxohugocamilo@gmail.com>
2026-06-03 16:55:57 -0400

    Correccion de Modelo AccumulatedPurchase

--- app/Models/AccumulatedPurchase.php
+++ app/Models/AccumulatedPurchase.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 
 class AccumulatedPurchase extends Model
 {
