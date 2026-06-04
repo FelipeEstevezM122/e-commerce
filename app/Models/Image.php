19bf77e55e3ba34878dd9a33a852ff22dba538d4
FelipeEstevezM122 <urimuck@gmail.com>
2026-06-04 12:22:43 -0400

    Model imagen

---
+++ app/Models/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'public_id',
+        'cloudinary_url',
+        'original_name',
+        'size',
+        'format'
+    ];
+}
