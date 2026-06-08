87d86c6f0204f24ba2b07bcad29ca740a0b2d7a3
FelipeEstevezM122 <urimuck@gmail.com>
2026-06-07 20:08:52 -0400

    Factori Orderitem actualizado

--- database/factories/OrderItemFactory.php
+++ database/factories/OrderItemFactory.php
@@ -11,18 +11,16 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class OrderItemFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
+        // Usa un producto existente en BD (ProductFactory fue eliminado)
+        $product = Product::inRandomOrder()->first();
+
         return [
-            'order_id' => Order::factory(),
-            'product_id' => Product::factory(),
-            'quantity' => $this->faker->numberBetween(1, 10),
-            'price_when_ordered' => $this->faker->randomFloat(2, 5, 500),
+            'order_id'           => Order::factory(),
+            'product_id'         => $product?->id,
+            'quantity'           => $this->faker->numberBetween(1, 10),
+            'price_when_ordered' => $product?->base_price ?? $this->faker->randomFloat(2, 50, 2000),
         ];
     }
 }
